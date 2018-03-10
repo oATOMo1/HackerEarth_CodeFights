4f9bd5634b3d730be3351f7712316e95811ca7d1
Aryan Bakshi <30727542+oATOMo1@users.noreply.github.com>
2018-03-11 01:27:30 +0530

    Added Comments

--- predicting_final_exam_ranks.py
+++ predicting_final_exam_ranks.py
@@ -1,15 +1,17 @@
+# Calculate GCD ( Greatest Common Divisor ) of x and y using EUCLIDEAN ALGORITHM
 def gcd(x,y):
     while(y):
         x,y = y, x%y
     return x
 	
-
+# Calculate number of numbers that are relatively prime within the range [1, current]
 def finalExamRank(current):
-    counter = 0
+    counter = 0 # for counting the numbers
     for i in range(1, current):
-        if gcd(i, current) == 1:
-            counter += 1
-    return counter
+        if gcd(i, current) == 1: # function returns 1 if numbers are relatively prime
+            counter += 1 # incrementing counter
+    return counter # returning the max numbers relatively prime
 
+# Asking input and calling our functions
 for i in range(0, int(input())):
     print(finalExamRank(int(input())))
